905b38cc7a616f26651b528863faf0be60df9a30
Alexey Kochkov <avik_mt-302@mail.ru>
2022-11-23 15:46:43 +0300

    homework 01 compleat

---
+++ lesson01/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+        println("Hello!")
+        println("Pinkie!")
+        println("Hello!")
+//      можно пренебречь именем Брейна
+//      println("Brain!")
+        print("Hey, Brain,")
+        println("what are we going to do tonight?")
+//        print("The same")
+//        print("as always,Pinkie…")
+        println("Let's try to take over the world!!")
+    }
