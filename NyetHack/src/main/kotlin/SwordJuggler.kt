727821e93044f1e4d233bf128014813d15a266d8
Alexey Kochkov <avik_mt-302@mail.ru>
2022-12-19 10:16:40 +0300

    version 04

--- NyetHack/src/main/kotlin/SwordJuggler.kt
+++ NyetHack/src/main/kotlin/SwordJuggler.kt
@@ -4,10 +4,20 @@ fun main() {
     if (isJugglingProficient) {
         swordsJuggling = 2
     }
-    proficiencyCheck(swordsJuggling)
-    swordsJuggling = swordsJuggling!!.plus(1)
+//    proficiencyCheck(swordsJuggling)
+//    swordsJuggling = swordsJuggling!!.plus(1)
+//    println("You juggle $swordsJuggling swords!")
+    try {
+        proficiencyCheck(swordsJuggling)
+        swordsJuggling = swordsJuggling!!.plus(1)
+    } catch (e: Exception) {
+        println(e)
+    }
     println("You juggle $swordsJuggling swords!")
 }
 fun proficiencyCheck(swordsJuggling: Int?) {
-    swordsJuggling ?: throw IllegalStateException("Player cannot juggle swords")
+//    swordsJuggling ?: throw IllegalStateException("Player cannot juggle swords")
+    swordsJuggling ?: throw UnskilledSwordJugglerException()
 }
+class UnskilledSwordJugglerException() :
+    IllegalStateException("Player cannot juggle swords")
